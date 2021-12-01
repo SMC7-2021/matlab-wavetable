9ef6f3b265bdfd690a3cd1466e46041aefa2d35f
vwesterlund <xwester@gmail.com>
2021-12-01 21:39:27 +0100

    Wavetable Outputs

--- sota/sota.m
+++ sota/sota.m
@@ -1,7 +1,8 @@
 clear; close all; clc;
 
-addpath('../helpers');
-[x, Fs] = audioread('./surge_sine_c5_long.aif');
+addpath('./helpers');
+[x, Fs] = audioread('wavetable_saw_c8.wav');
 % [x, Fs] = audioread('./retro_square_c7.aif');
+x = normalize(x)/2;
 tfPlot(x(20000:end, 1), Fs, .01);
-figure; spectrogram(x(:,1), 512, 64, 512, Fs, 'yaxis');
+figure; spectrogram(x(:,1), 512, 64, 512, Fs, 'yaxis'); 
